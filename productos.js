6676a47c7459dcfbbe59dfe2286e49f67b603419
Nicolás Cammarota <76768475+Nikong22@users.noreply.github.com>
2021-09-09 09:38:04 -0300

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,24 @@
+const productos = [
+    {
+        id: 1,
+        title:"Netflix",
+        price:500,
+        thumbnail:"http://netflix"
+    },
+
+    {
+        id: 2,
+        title:"Disney",
+        price:550,
+        thumbnail:"http://disney"
+    },
+
+    {
+        id: 3,
+        title:"Paramount",
+        price:555,
+        thumbnail:"http://paramount"
+    }
+];
+  export default productos;
+  
